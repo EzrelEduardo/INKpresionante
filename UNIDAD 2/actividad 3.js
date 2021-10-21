009bf9ef5e788ea43469d0493993e32df09308b0
EzrelEduardo <89996318+EzrelEduardo@users.noreply.github.com>
2021-10-21 18:22:51 -0500

    Add files via upload

---
+++ UNIDAD 2/actividad 3.js
@@ -0,0 +1,21 @@
+var letras = prompt("ingrese operacion")
+var numero1 = 10
+var numero2 = 5
+suma (10+15);
+resta (10-15);
+var resultado = 15
+var resultado2 = -5
+
+switch(letras){
+    case "suma":
+        function suma();{
+            console.log(resultado)
+        }
+break;
+case "resta":
+    function resta();{
+        console.log(resultado2)
+    }
+    break;
+}
+
